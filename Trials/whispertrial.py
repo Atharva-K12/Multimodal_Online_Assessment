cf2f8484fa1ee6c56bbcbe821c92ad2ca4baae41
Atharva Kathale <atharva.kathale@gmail.com>
2022-10-17 14:30:20 +0530

    Update whispertrial.py

--- Trials/whispertrial.py
+++ Trials/whispertrial.py
@@ -1,21 +1,17 @@
 import whisper
 
+
 # use openAI whisper to convert speech to text
 
 def useWhisper(audioFile):
     model = whisper.load_model("base")
     result = model.transcribe(audioFile,fp16=False)
     print(result["text"])
-
-
-def useWhisperLive():
-    model = whisper.load_model("base")
     
-    result = model.transcribe_live()
-    print(result["text"])
+
 if __name__ == "__main__":
-    useWhisper("./JettMatchStartAscent.mp3")
-    useWhisperLive()
+    useWhisper("m1.mp3")
+
 
 
 
