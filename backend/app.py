5c8f65b1e0a1bed839316116b26abdde56a9dbd9
Sahil-18 <sahilpurohit18@gmail.com>
2022-12-22 19:30:54 +0530

    Middleware file created and updated

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,8 @@
 import os
 import bson.json_util as json_util
 import bcrypt
-from functools import wraps
 from flask import Flask, request, session, make_response, jsonify
-from werkzeug.utils import secure_filename
 from flask_cors import CORS
-import moviepy.editor as mp
 import jwt
 from useWhisper import audioToText
 # from videoEyeTrack import VideoEyeTracker
@@ -13,6 +10,7 @@ from pymongo import MongoClient
 from sentenceMatch import sentence_match , sentence_scoring_metric
 from score import scoring_criteria
 from recommender1 import Question
+from middleware import token_validation
 
 UPLOAD_FOLDER = './uploads'
 
@@ -64,26 +62,6 @@ def Login():
         else:   
             return "Username does not exist"
 
-def token_validation(func):
-    @wraps(func)
-    def validate(*args, **kwargs):
-        token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'message':'Token is missing'}), 401
-
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-            current_user = db.users.find_one({'public_id':data['public_id']})
-            if not current_user:
-                return jsonify({'message':'Token is invalid'}), 401
-        except:
-            return jsonify({'message':'Token is invalid'}), 401
-        return func(current_user, *args, **kwargs)
-    
-    return validate
-
 
 @app.route("/Logout", methods=['GET'])
 @token_validation
@@ -91,45 +69,6 @@ def Logout(current_user):
     session.pop('username', None)
     return "Logout successful"
 
-
-@app.route("/upload", methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return "No file part"
-        file = request.files['file']
-        if file.filename == '':
-            return "No selected file"
-        if file:
-            # Saved file is mp4 file
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # video = mp.VideoFileClip(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # video.audio.write_audiofile(os.path.join(app.config['UPLOAD_FOLDER'], filename.split(".")[0] + ".wav"))
-            # # grade function to be called here
-
-            #Get answer from database
-            #client = MongoClient("mongodb+srv://test:test12345@fypdb.11jbtg4.mongodb.net/?retryWrites=true&w=majority")
-            #db = client.get_database('fyp')
-            #records= db.questionBank
-            question = request.form['question']
-            question = records.find({"question":question})
-            answer = json_util.dumps(question[0]["answer"])
-
-            # Audio to text conversion and sentence matching
-            app.config['FILE_PATH'] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            sentence_cosine_score = sentence_match(audioToText(app.config['FILE_PATH']),answer)
-            text_score = sentence_scoring_metric(sentence_cosine_score)
-            # # Video analysis function to be called here
-            video_score = 0
-            # video_score = VideoEyeTracker(app.config['FILE_PATH']).plagpercent
-            # Final score to be calculated here
-            final_score = scoring_criteria(text_score,video_score)
-            q.asked_questions.append({'Question':question[0], 'Score' :final_score})
-            score = {"score":final_score,"text_score":text_score, "video_score":video_score}
-            
-            json_return_score = json_util.dumps(score)
-            return json_return_score
             
 @app.route("/get-question", methods=['GET'])
 def get_question():
@@ -148,7 +87,7 @@ def get_question():
 
 # Multi user support
 @app.route("/upload", methods=['POST'])
-@token_validation
+@token_validation()
 def upload_file(current_user):
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -201,7 +140,6 @@ def upload_video(current_user):
         if file:
             # Saved file is mp4 file
             username = current_user['username']
-            qid = request.form['qid']
             filename = username + ".mp4"
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return "File uploaded successfully"
