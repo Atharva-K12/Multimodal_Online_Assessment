7f7609c216ddc47d4eef97cb723ed40becf7325f
meetagrawal09 <agrawalmeet91@gmail.com>
2023-01-04 20:28:48 +0530

    cleaned code

--- backend/useWhisper.py
+++
@@ -1,7 +0,0 @@
-import whisper
-
-def audioToText(audioFile):
-    model = whisper.load_model("base")
-    result = model.transcribe(audioFile,fp16=False)
-    print(result['text'])
-    return result['text']
