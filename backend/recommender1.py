d69ae1739dfa9a622a7ab315121869ac0ce8a718
Sahil-18 <sahilpurohit18@gmail.com>
2023-02-09 13:39:05 +0530

    results

--- backend/recommender1.py
+++ backend/recommender1.py
@@ -1,8 +1,15 @@
 import random
-
 import pandas as pd
 from sentenceMatch import sentence_encoding
 import numpy as np
+import spacy
+import tracemalloc
+
+nlp = spacy.load('en_core_web_sm')
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
 
 class Question:
     def __init__(self, question_list):
@@ -222,20 +229,85 @@ class QuestionHandler:
     def add_question(self, question):
         self.questions.append(Vectorize_Questions([question]))
     
+class  RecommenderCluster2:
+    def __init__(self, question_list):
+        self.question_list = question_list
+        self.vectorized_questions = Vectorize_Questions(self.question_list).vectorized_questions
+        self.recommended_questions = []
+        self.k = 5
+        self.distance_metric = self.cosine_similarity
+        self.threshold = 0.7
+        self.recommend()
+    
+    def euclidean_distance(self, vector1, vector2):
+        return np.linalg.norm(vector1 - vector2)
+    
+    def cosine_similarity(self, vector1, vector2):
+        return np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+
+    def cluster(self):
+        clusters = []
+        for vectorized_question in self.vectorized_questions:
+            if len(clusters) == 0:
+                clusters.append([vectorized_question])
+            else:
+                for cluster in clusters:
+                    if self.distance_metric(cluster[0]['vector'], vectorized_question['vector']) > self.threshold:
+                        cluster.append(vectorized_question)
+                        break
+                else:
+                    clusters.append([vectorized_question])
+        return clusters
+    
+    def recommend(self, question = None):
+        if question is None:
+            question = self.vectorized_questions[np.random.randint(0, len(self.vectorized_questions))]
+            self.recommended_questions.append(question)
+            return
+        clusters = self.cluster()
+        cluster_medians = []
+        for cluster in clusters:
+            cluster_medians.append(self.find_median(cluster))
+        distances = []
+        question = [{'question': question}]
+        question1 = Vectorize_Questions(question).vectorized_questions[0]
+        for i,cluster_median in enumerate(cluster_medians):
+            distance = self.distance_metric(cluster_median['vector'], question1['vector'])
+            distances.append({'question':cluster_median['question'],'vector': cluster_median['vector'], 'distance':distance,'index':i})
+        distances.sort(key=lambda x: x['distance'])
+        index = distances[0]['index']
+        flag = True
+        while(flag):
+            for member in clusters[index]:
+                if member not in self.recommended_questions:
+                    self.recommended_questions.append(member)
+                    flag = False
+                    break
+        return self.recommended_questions[-1]
+    
+    def find_median(self, cluster):
+        distances = []
+        for vectorized_question in cluster:
+            distance = self.distance_metric(cluster[0]['vector'], vectorized_question['vector'])
+            distances.append({'question':vectorized_question['question'],'vector': vectorized_question['vector'], 'distance':distance})
+        distances.sort(key=lambda x: x['distance'])
+        return distances[len(distances)//2]
     
     
 
 def RecomTest():
     #read questions from database
+    tracemalloc.start()
     questions= pd.read_csv('../Trials/Question Bank.csv')
     questions=questions.to_dict('records')
-    c2=RecommenderCluster(questions)
+    c2=RecommenderCluster2(questions)
     query_question =c2.recommended_questions[0]
+    print("Memory Usage: ",tracemalloc.get_traced_memory()[0]/10**6,"MB")
     print('query_question')
     print(query_question['question'])
     for i in range(4):
-        query_question = c2.recommend(query_question)
-        print(i)
+        answer = input('Enter answer: ')
+        query_question = c2.recommend(answer)
         print('query_question')
         print(query_question['question'])
     # q = {'question':'What is a virtual function?'}
@@ -243,6 +315,22 @@ def RecomTest():
     # ans = 'A virtual function is a member function of a class, and its functionality can be overridden in its derived class. This function can be implemented by using a keyword called virtual, and it can be given during function declaration'
     # print(c2.recommend(ans))   
 
+def NERTest():
+    questions= pd.read_csv('../Trials/Question Bank.csv')
+    questions=questions.to_dict('records')
+    for question in questions:
+        print(question['question'])
+        words = word_tokenize(question['question'])
+        stopword = set(stopwords.words('english'))
+        words = [word for word in words if word not in stopword]
+        fdist = FreqDist(words)
+        print(fdist.most_common(2))
+
+        document = nlp(question['question'])
+        entities = [entity.text for entity in document.ents]
+        print(entities)
+
 if __name__ == "__main__":
     RecomTest()
+    # NERTest()
         
