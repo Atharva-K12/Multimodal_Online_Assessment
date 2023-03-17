a4cf169bf6c596ba00ebff30834601949b2fdd4e
Sahil-18 <sahilpurohit18@gmail.com>
2023-03-17 14:40:29 +0530

    Other login + Test and enrollment Tables

---
+++ backend/Models/enrollment.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from flask import current_app
+from flask import make_response, jsonify
+from Models.test import Test
+from Models.student import Student
+
+class Enrollment:
+    def __init__(self):
+        self.client = MongoClient(current_app.config['MONGO_URI'])
+        self.db = self.client['fyp']
+        self.collection = self.db['enrollments']
+
+    def get_enrollments(self,student_name):
+        student_id = Student().get_student_id(student_name)
+        tests = self.collection.find({'student_id':student_id})
+        if tests is None:
+            return make_response(jsonify({'message': 'Not enrolled in any test'}), 404)
+        test_list = []
+        for test in tests:
+            test_list.append(Test().get_test(test['test_id']))
+        return make_response(jsonify({'tests':test_list}), 200)
+
+    def enroll(self, data):
+        test_id = Test().get_test_id(data['test_name'])
+        student_id = Student().get_student_id(data['student_name'])
+        if self.collection.find_one({'student_id':student_id, 'test_id':test_id}):
+            return make_response(jsonify({'message': 'Already enrolled in test'}), 401)
+        data['test_id'] = test_id
+        data['student_id'] = student_id
+        data.pop('student_name')
+        data.pop('test_name')
+        self.collection.insert_one(data)
+        return make_response(jsonify({'message': 'Enrolled in test successfully'}), 200)
--- backend/app/Login/login.py
+++ backend/app/Login/login.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request,current_app, make_response, jsonify
 import bcrypt,jwt
 from Models.student import Student
-from app.middelware import token_validation
+from Models.teacher import Teacher
+from Models.admin import Admin
 
 login = Blueprint('login', __name__)
 
 
 @login.route('/student-register', methods=['POST'])
 def student_register():
-    # Code to register student
     if request.method == 'POST':
         data = request.get_json()
         data['password'] = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
@@ -16,26 +16,17 @@ def student_register():
     
 
 
-# @login.route('/teacher-register', methods=['POST'])
-# def teacher_register():
-#     # Code to register teacher
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         username = data['username']
-#         password = data['password']
-#         user = db.teacher.find_one({'username': username})
-#         if user:
-#             return make_response(jsonify({'message': 'Username already exists'}), 401)
-#         else:
-#             password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-#             db.teacher.insert_one({'username': username, 'password': password, 'email': request.form['email'], 'name': request.form['name'], 'status':'pending' })
-#             return make_response(jsonify({'message': 'Teacher registered successfully and status pending'}), 200)
+@login.route('/teacher-register', methods=['POST'])
+def teacher_register():
+    if request.method == 'POST':
+        data = request.get_json()
+        data['password'] = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+        return Teacher().add_teacher(data)
 
 
 
 @login.route('/student-login', methods=['POST'])
 def student_login():
-    # Code to login student
     if request.method == 'POST':
         data = request.get_json()
         username = data['username']
@@ -52,37 +43,36 @@ def student_login():
             return make_response(jsonify({'message': 'Invalid username'}), 401)
 
 
-# @login.route('/teacher-login', methods=['POST'])
-# def teacher_login():
-#     # Code to login teacher
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         username = data['username']
-#         password = data['password']
-#         user = db.teacher.find_one({'username': username})
-#         if user:
-#             if bcrypt.hashpw(password.encode('utf-8'), user['password'].encode('utf-8')) == user['password'].encode('utf-8'):
-#                 token = jwt.encode({'username': username, 'role': 'teacher'}, current_app.config['SECRET_KEY'])
-#                 return make_response(jsonify({'token':token}), 200)
-#             else:
-#                 return make_response(jsonify({'message': 'Invalid password'}), 401)
-#         else:
-#             return make_response(jsonify({'message': 'Invalid username'}), 401)
+@login.route('/teacher-login', methods=['POST'])
+def teacher_login():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user = Teacher().find_one({'username': username})
+        if user:
+            if bcrypt.hashpw(password.encode('utf-8'), user['password']) == user['password']:
+                token = jwt.encode({'username': username, 'role': 'teacher'}, current_app.config['SECRET_KEY'])
+                return make_response(jsonify({'token':token, 'username': username}), 200)
+            else:
+                return make_response(jsonify({'message': 'Invalid password'}), 401)
+        else:
+            return make_response(jsonify({'message': 'Invalid username'}), 401)
 
 
-# @login.route('/admin-login', methods=['POST'])
-# def admin_login():
-#     # Code to login teacher
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         username = data['username']
-#         password = data['password']
-#         user = db.admin.find_one({'username': username})
-#         if user:
-#             if bcrypt.hashpw(password.encode('utf-8'), user['password'].encode('utf-8')) == user['password'].encode('utf-8'):
-#                 token = jwt.encode({'username': username, 'role': 'admin'}, current_app.config['SECRET_KEY'])
-#                 return make_response(jsonify({'token':token}), 200)
-#             else:
-#                 return make_response(jsonify({'message': 'Invalid password'}), 401)
-#         else:
-#             return make_response(jsonify({'message': 'Invalid username'}), 401)
+@login.route('/admin-login', methods=['POST'])
+def admin_login():
+    # Code to login teacher
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user = Admin().find_one({'username': username})
+        if user:
+            if bcrypt.hashpw(password.encode('utf-8'), user['password']) == user['password']:
+                token = jwt.encode({'username': username, 'role': 'admin'}, current_app.config['SECRET_KEY'])
+                return make_response(jsonify({'token':token, 'username': username}), 200)
+            else:
+                return make_response(jsonify({'message': 'Invalid password'}), 401)
+        else:
+            return make_response(jsonify({'message': 'Invalid username'}), 401)
