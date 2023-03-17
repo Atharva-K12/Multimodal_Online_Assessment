0496b7eeec339d82b1876f7bdc0bcfd74804815f
Atharva Kathale <atharva.kathale@gmail.com>
2023-03-17 21:38:15 +0530

    config structure

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,33 @@
-SECRET_KEY = 'xyz'
-MONGO_URI = 'mongodb://Sahil_Purohit:#Pass4mongodb@ac-hifbqdl-shard-00-00.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-01.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-02.11jbtg4.mongodb.net:27017/?ssl=true&replicaSet=atlas-bzogsd-shard-0&authSource=admin&retryWrites=true&w=majority'
+# SECRET_KEY = 'xyz'
+# # MONGO_URI = 'mongodb://Sahil_Purohit:#Pass4mongodb@ac-hifbqdl-shard-00-00.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-01.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-02.11jbtg4.mongodb.net:27017/?ssl=true&replicaSet=atlas-bzogsd-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+# # from dotenv import load_dotenv
+# import os
+
+# # load_dotenv()
+
+# # MONGO_URI = 'mongodb://'+os.getenv('MONGO_USERNAME')+':'+os.getenv('MONGO_PASSWORD')+'@ac-hifbqdl-shard-00-00.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-01.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-02.11jbtg4.mongodb.net:27017/?ssl=true&replicaSet=atlas-bzogsd-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+
+# # print(MONGO_URI)
+
+
+# from dotenv import load_dotenv, main
+
+# try:
+#     load_dotenv()
+# except main.DotEnvError as e:
+#     print(f"Error loading .env file: {e}")
+
+# MONGO_URI = f"mongodb://{os.getenv('MONGO_USERNAME')}:{os.getenv('MONGO_PASSWORD')}@ac-hifbqdl-shard-00-00.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-01.11jbtg4.mongodb.net:27017,ac-hifbqdl-shard-00-02.11jbtg4.mongodb.net:27017/?ssl=true&replicaSet=atlas-bzogsd-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+
+
+import yaml
+
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+mongo_uri = f"mongodb://{config['mongo_username']}:{config['mongo_password']}@{config['mongo_host']}/?{config['mongo_options']}"
+
+print(mongo_uri)
