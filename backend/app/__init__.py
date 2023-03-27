9593e50191c20a1e5645af78dfbb422fabb7e14c
Atharva Kathale <atharva.kathale@gmail.com>
2023-03-27 10:39:53 +0530

    vectoriser

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.Login.login import login
 from app.Student.student import student
+from app.Functionalities.dbVectorise import db
 from flask_cors import CORS
 
 def Create_app(config):
@@ -9,5 +10,6 @@ def Create_app(config):
     app.config.from_pyfile(config)
     app.register_blueprint(login)
     app.register_blueprint(student)
+    app.register_blueprint(db)
     return app
 
