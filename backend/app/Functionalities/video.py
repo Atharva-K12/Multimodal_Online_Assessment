26e9a7673b048f62616d15de3df0f7e2c2275e28
Kals-13 <kalyanisainis1310@gmail.com>
2023-04-22 22:05:07 +0530

    modified:   backend/app/Functionalities/video.py

--- backend/app/Functionalities/video.py
+++ backend/app/Functionalities/video.py
@@ -109,24 +109,21 @@ def headpose_detect(video_path):
     mp_face_mesh = mp.solutions.face_mesh
     face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
     cap = cv2.VideoCapture(video_path)
+    result = dict()
+    for key in ['left', 'right', 'center', 'down', 'up']:
+        result[key] = 0
 
     while cap.isOpened():
         _, image = cap.read()
 
-        # Flip the image horizontally for a later selfie-view display
-        # Also convert the color space from BGR to RGB
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 
-        # To improve performance
         image.flags.writeable = False
         
-        # Get the result
         results = face_mesh.process(image)
-        
-        # To improve performance
+
         image.flags.writeable = True
         
-        # Convert the color space from RGB to BGR
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         img_h, img_w, img_c = image.shape
@@ -143,52 +140,38 @@ def headpose_detect(video_path):
 
                         x, y = int(lm.x * img_w), int(lm.y * img_h)
 
-                        # Get the 2D Coordinates
                         face_2d.append([x, y])
 
-                        # Get the 3D Coordinates
                         face_3d.append([x, y, lm.z])       
                 
-                # Convert it to the NumPy array
                 face_2d = np.array(face_2d, dtype=np.float64)
-
-                # Convert it to the NumPy array
                 face_3d = np.array(face_3d, dtype=np.float64)
 
-                # The camera matrix
                 focal_length = 1 * img_w
 
                 cam_matrix = np.array([ [focal_length, 0, img_h / 2],
                                         [0, focal_length, img_w / 2],
                                         [0, 0, 1]])
 
-                # The Distance Matrix
                 dist_matrix = np.zeros((4, 1), dtype=np.float64)
 
-                # Solve PnP
                 success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
 
-                # Get rotational matrix
                 rmat, jac = cv2.Rodrigues(rot_vec)
 
-                # Get angles
                 angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
 
-                # Get the y rotation degree
                 x = angles[0] * 360
                 y = angles[1] * 360
 
-                # print(y)
-
-                # See where the user's head tilting
                 if y < -10:
-                    text = "Looking Left"
+                    result['left'] += 1
                 elif y > 10:
-                    text = "Looking Right"
+                    result['right'] += 1
                 elif x < -10:
-                    text = "Looking Down"
+                    result['down'] += 1
                 else:
-                    text = "Forward"
+                    result['centre'] += 1
     cap.release()
     
     return 0
@@ -202,10 +185,8 @@ def gaze_tracking(video_path):
         result[key] = 0
 
     while webcam.isOpened():
-        # We get a new frame from the webcam
         _, frame = webcam.read()
 
-        # We send this frame to GazeTracking to analyze it
         gaze.refresh(frame)
 
         frame = gaze.annotated_frame()
