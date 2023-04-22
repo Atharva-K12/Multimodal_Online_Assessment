2819bf63cad4cfe25e41737cfca21d942abbd832
Kals-13 <kalyanisainis1310@gmail.com>
2023-04-22 22:04:56 +0530

    complete video analysis code

--- backend/app/Functionalities/video.py
+++ backend/app/Functionalities/video.py
@@ -7,6 +7,9 @@ from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
 import imutils
+from gaze_tracking import GazeTracking
+import mediapipe as mp
+import numpy as np
 
 def face_detect(video_path):
     cam = cv2.VideoCapture(video_path)
@@ -26,7 +29,7 @@ def face_detect(video_path):
     cam.release()
     return frame_count
         
-
+'''
 def eye_aspect_ratio(eye):
 	# compute the euclidean distances between the two sets of
 	# vertical eye landmarks (x, y)-coordinates
@@ -100,12 +103,125 @@ def eye_blink_detect(filepath):
                 # reset the eye frame counter
                 COUNTER = 0
     return blink
+'''
 
-def headpose_detect(img):
+def headpose_detect(video_path):
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    cap = cv2.VideoCapture(video_path)
+
+    while cap.isOpened():
+        _, image = cap.read()
+
+        # Flip the image horizontally for a later selfie-view display
+        # Also convert the color space from BGR to RGB
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+        # To improve performance
+        image.flags.writeable = False
+        
+        # Get the result
+        results = face_mesh.process(image)
+        
+        # To improve performance
+        image.flags.writeable = True
+        
+        # Convert the color space from RGB to BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        img_h, img_w, img_c = image.shape
+        face_3d = []
+        face_2d = []
+
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                for idx, lm in enumerate(face_landmarks.landmark):
+                    if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                        if idx == 1:
+                            nose_2d = (lm.x * img_w, lm.y * img_h)
+                            nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 8000)
+
+                        x, y = int(lm.x * img_w), int(lm.y * img_h)
+
+                        # Get the 2D Coordinates
+                        face_2d.append([x, y])
+
+                        # Get the 3D Coordinates
+                        face_3d.append([x, y, lm.z])       
+                
+                # Convert it to the NumPy array
+                face_2d = np.array(face_2d, dtype=np.float64)
+
+                # Convert it to the NumPy array
+                face_3d = np.array(face_3d, dtype=np.float64)
+
+                # The camera matrix
+                focal_length = 1 * img_w
+
+                cam_matrix = np.array([ [focal_length, 0, img_h / 2],
+                                        [0, focal_length, img_w / 2],
+                                        [0, 0, 1]])
+
+                # The Distance Matrix
+                dist_matrix = np.zeros((4, 1), dtype=np.float64)
+
+                # Solve PnP
+                success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+
+                # Get rotational matrix
+                rmat, jac = cv2.Rodrigues(rot_vec)
+
+                # Get angles
+                angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
+                # Get the y rotation degree
+                x = angles[0] * 360
+                y = angles[1] * 360
+
+                # print(y)
+
+                # See where the user's head tilting
+                if y < -10:
+                    text = "Looking Left"
+                elif y > 10:
+                    text = "Looking Right"
+                elif x < -10:
+                    text = "Looking Down"
+                else:
+                    text = "Forward"
+    cap.release()
+    
     return 0
 
-def gaze_tracking(img):
-    return 0
+def gaze_tracking(video_path):
+    gaze = GazeTracking()
+    webcam = cv2.VideoCapture(video_path)
+    result = dict()
+    
+    for key in ['left', 'right', 'center', 'blink']:
+        result[key] = 0
+
+    while webcam.isOpened():
+        # We get a new frame from the webcam
+        _, frame = webcam.read()
+
+        # We send this frame to GazeTracking to analyze it
+        gaze.refresh(frame)
+
+        frame = gaze.annotated_frame()
+
+        if gaze.is_blinking():
+            result['blink'] += 1
+        elif gaze.is_right():
+            result['right'] += 1
+        elif gaze.is_left():
+            result['left'] += 1
+        elif gaze.is_center():
+            result['center'] += 1
+ 
+    webcam.release()     
+    return result
+            
 
 # Perform all malpractice checks for video analysis
 def video_analysis(video_path):
@@ -114,7 +230,9 @@ def video_analysis(video_path):
     for key in keys:
         scores[key] = None
     scores['face'] = face_detect(video_path)
-    scores['blink'] = eye_blink_detect(video_path)
+    #scores['blink'] = eye_blink_detect(video_path)
+    scores['gaze'] = gaze_tracking(video_path)
+    scores['headpose'] = headpose_detect(video_path)
 
     return scores
         
